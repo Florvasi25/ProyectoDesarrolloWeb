2d9d4dd8599214b669c6c4eeff6952d4d5ef8bcd
florencia.vasiliauskas <florvasiliauskas@gmail.com>
2022-05-26 22:01:45 -0300

    Prueba 3

--- form.php
+++ form.php
@@ -7,11 +7,11 @@ $telefono = $_POST ['phone'];
 $email = $_POST {'email'};
 
 $mensaje = "Este mensaje fue enviado por " . $nombreTutor . ",\r\n";
-$mensaje = "Está interesado en inscribir a " . $nombreAlumno . ",\r\n";
-$mensaje = "Quien tiene una edad de " . $edad . ",\r\n";
-$mensaje = "El teléfono de contacto es" . $telefono . ",\r\n";
-$mensaje = "y el email es " . $email . ",\r\n";
-$mensaje = "Enviado el " . date('d/m/Y', time());
+$mensaje .= "Está interesado en inscribir a " . $nombreAlumno . ",\r\n";
+$mensaje .= "Quien tiene una edad de " . $edad . ",\r\n";
+$mensaje .= "El teléfono de contacto es" . $telefono . ",\r\n";
+$mensaje .= "y el email es " . $email . ",\r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
 
 $destinatario = 'florvasiliauskas@gmail.com';
 $asunto = 'Solicitud de información';
